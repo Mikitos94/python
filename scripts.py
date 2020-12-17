393a5a7e2cdb79bf83f9573f915cf54924106f4b
mikit <mikitos94@gmail.com>
2020-12-17 10:56:37 +0300

    homework 12.7.3. added input()

--- scripts.py
+++ scripts.py
@@ -1,3 +1,5 @@
+
+
 input('Нажми ENTER  чтобы начать')
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 print('На данный момент проценты у банка по вкладам такие:' + (str(per_cent)))
@@ -6,3 +8,4 @@ per_cent_list=list(per_cent_values)
 money=(float(input('Введи сумму, которую Вы хотите положить:')))
 deposit=[money*i*0.1 for i in per_cent_list]
 print('Максимальная сумма, которую Вы можете заработать: ' + (str(max(deposit))))
+input()
